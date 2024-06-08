6f5428d21457ced8118bcb93c208937bd2cbc891
michaelthecircle <90508054+michaelthecircle@users.noreply.github.com>
2024-06-08 07:25:54 +0300

    sending and getting messages from crypting server

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,21 +9,34 @@ const connectingElement = document.querySelector('.connecting');
 const chatArea = document.querySelector('#chat-messages');
 const logout = document.querySelector('#logout');
 
+const P = 23n;
+const G = 5n;
+
+function getRandomBigInt(max) {
+    let randomBigInt;
+    do {
+        randomBigInt = BigInt(Math.floor(Math.random() * Number(max)));
+    } while (randomBigInt === 0n);
+    return randomBigInt;
+}
+
+function generatePrivateKey() {
+    const maxRandom = P - 1n;
+    return getRandomBigInt(maxRandom);
+}
+
+function generatePublicKey(privateKey) {
+    return G ** privateKey % P;
+}
+
+function generateSharedKey(publicKey, privateKey) {
+    return publicKey ** privateKey % P;
+}
+
 let stompClient = null;
 let nickname = null;
 let fullname = null;
 let selectedUserId = null;
-
-const p = 23; // Простое число для алгоритма Диффи-Хеллмана
-const g = 5;  // Примитивный корень по модулю p
-let privateKey = null;
-let publicKey = null;
-let sharedSecret = null;
-
-function generateDHKeys() {
-    privateKey = Math.floor(Math.random() * 100) + 1;
-    publicKey = Math.pow(g, privateKey) % p;
-}
 
 function connect(event) {
     nickname = document.querySelector('#nickname').value.trim();
@@ -42,39 +55,15 @@ function connect(event) {
 }
 
 function onConnected() {
-    generateDHKeys(); // Генерация ключей Диффи-Хеллмана
-    performDHKeyExchange(); // Обмен публичными ключами
-
     stompClient.subscribe(`/user/${nickname}/queue/messages`, onMessageReceived);
     stompClient.subscribe(`/user/public`, onMessageReceived);
 
-    stompClient.subscribe(`/user/${nickname}/queue/publicKey`, onPublicKeyReceived);
-
-    // register the connected user
     stompClient.send("/app/user.addUser",
         {},
-        JSON.stringify({ nickName: nickname, fullName: fullname, status: 'ONLINE' })
+        JSON.stringify({nickName: nickname, fullName: fullname, status: 'ONLINE'})
     );
     document.querySelector('#connected-user-fullname').textContent = fullname;
     findAndDisplayConnectedUsers().then();
-}
-
-function performDHKeyExchange() {
-    stompClient.send("/app/user.sendPublicKey",
-        {},
-        JSON.stringify({ publicKey: publicKey })
-    );
-}
-
-function onPublicKeyReceived(payload) {
-    const message = JSON.parse(payload.body);
-    const receivedPublicKey = message.publicKey;
-    computeSharedSecret(receivedPublicKey);
-}
-
-function computeSharedSecret(receivedPublicKey) {
-    sharedSecret = Math.pow(receivedPublicKey, privateKey) % p;
-    console.log("Общий секрет вычислен:", sharedSecret);
 }
 
 async function findAndDisplayConnectedUsers() {
@@ -100,7 +89,7 @@ function appendUserElement(user, connectedUsersList) {
     listItem.id = user.nickName;
 
     const userImage = document.createElement('img');
-    userImage.src = '../img/images.jpeg';
+    userImage.src = '../img/pivo.jpeg';
     userImage.alt = user.fullName;
 
     const usernameSpan = document.createElement('span');
@@ -134,7 +123,6 @@ function userItemClick(event) {
     const nbrMsg = clickedUser.querySelector('.nbr-msg');
     nbrMsg.classList.add('hidden');
     nbrMsg.textContent = '0';
-
 }
 
 function displayMessage(senderId, content) {
@@ -166,19 +154,62 @@ function onError() {
     connectingElement.style.color = 'red';
 }
 
-let selectedAlgorithm = "Twofish";
+let selectedAlgorithm = "";
+let encryptionMode = "";
+let padding = "";
 function selectEncryptionAlgorithm(algorithm) {
     selectedAlgorithm = algorithm;
 }
 
-function encrypt(messageContent, selectedAlgorithm) {
-    return messageContent + " (encrypted with " + selectedAlgorithm + ")";
+//DH
+let privateKeyAlice = 0n;
+let publicKeyAlice = 0n;
+let sharedKeyAlice = 0n;
+let privateKeyBob = 0n;
+let publicKeyBob = 0n;
+let sharedKeyBob = 0n;
+let statusHellman = 0;
+
+function setHellmanStatus(status) {
+    statusHellman = status;
 }
 
-function sendMessage(event) {
+async function encrypt(messageContent, selectedAlgorithm, sharedKey, timeStamp) {
+    var xhr = new XMLHttpRequest();
+    var url = "http://localhost:8090/encryptMsg";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    let result = "";
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            result = JSON.parse(xhr.responseText).content;
+        } else if (xhr.readyState === 4) {
+            console.error("Failed to encrypt message:", xhr.status, xhr.responseText);
+        }
+    };
+    var data = JSON.stringify({
+        key: timeStamp,
+        crypto_algorithm: selectedAlgorithm,
+        padding: "ZEROS",
+        cipher_mode: "CBC",
+        content: messageContent
+    });
+    console.log("Sending data:", data);
+    xhr.send(data);
+    return result;
+}
+
+async function sendMessage(event) {
     const messageContent = messageInput.value.trim();
     if (messageContent && stompClient) {
-        const encryptedMessage = encrypt(messageContent, selectedAlgorithm);
+        if (statusHellman === 0) {
+            privateKeyAlice = generatePrivateKey();
+            publicKeyAlice = generatePublicKey(privateKeyAlice);
+            setHellmanStatus(1);
+        } else if (statusHellman === 2) {
+            sharedKeyAlice = generateSharedKey(publicKeyBob, privateKeyAlice);
+        }
+        const encryptedMessage = await encrypt(messageContent, selectedAlgorithm, sharedKeyAlice, timeStamp);
         const chatMessage = {
             senderId: nickname,
             recipientId: selectedUserId,
@@ -186,16 +217,40 @@ function sendMessage(event) {
             algorithm: selectedAlgorithm,
             timestamp: new Date()
         };
+        console.log("СООБЩЕНИЕ ПРИШЛО НА ОТПРАВКУ");
+        console.log(encryptedMessage);
+        console.log("-------------");
         stompClient.send("/app/chat", {}, JSON.stringify(chatMessage));
-        displayMessage(nickname, encryptedMessage);
+        displayMessage(nickname, messageContent);
         messageInput.value = '';
     }
     chatArea.scrollTop = chatArea.scrollHeight;
     event.preventDefault();
 }
 
-function decrypt(content, selectedAlgorithm) {
-    return content + " (decrypted " + selectedAlgorithm + ")";
+function decrypt(content, selectedAlgorithm, shared, timeStamp) {
+    var xhr = new XMLHttpRequest();
+    var url = "http://localhost:8090/decryptMsg";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    let result = "";
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            result = JSON.parse(xhr.responseText).content;
+        } else if (xhr.readyState === 4) {
+            console.error("Failed to encrypt message:", xhr.status, xhr.responseText);
+        }
+    };
+    var data = JSON.stringify({
+        key: timeStamp,
+        crypto_algorithm: selectedAlgorithm,
+        padding: "ZEROS",
+        cipher_mode: "CBC",
+        content: messageContent
+    });
+    console.log("Sending data:", data);
+    xhr.send(data);
+    return result;
 }
 
 async function onMessageReceived(payload) {
@@ -203,8 +258,17 @@ async function onMessageReceived(payload) {
     console.log('Message received', payload);
     const message = JSON.parse(payload.body);
     if (selectedUserId && selectedUserId === message.senderId) {
-        selectedAlgorithm = message.algorithm;
-        const decryptedMessage = decrypt(message.content, selectedAlgorithm);
+        if (statusHellman === 1) {
+            privateKeyBob = generatePrivateKey();
+            publicKeyBob = generatePublicKey(privateKeyBob);
+            sharedKeyBob = generateSharedKey(publicKeyAlice, privateKeyBob);
+            setHellmanStatus(2);
+        }
+        selectEncryptionAlgorithm(message.algorithm);
+        console.log("СООБЩЕНИЕ ПРИШЛО");
+        console.log(message.content);
+        console.log("-------------");
+        const decryptedMessage = decrypt(message.content, selectedAlgorithm, sharedKeyBob, timeStamp);
         displayMessage(message.senderId, decryptedMessage);
         chatArea.scrollTop = chatArea.scrollHeight;
     }
@@ -223,15 +287,77 @@ async function onMessageReceived(payload) {
     }
 }
 
+const fileInput = document.getElementById('fileInput');
+const attachFileButton = document.getElementById('attachFileButton');
+
+attachFileButton.addEventListener('click', () => {
+    fileInput.click();
+});
+
+const refreshUsersButton = document.getElementById('refreshUsersButton');
+
+refreshUsersButton.addEventListener('click', async () => {
+    try {
+        await findAndDisplayConnectedUsers();
+        console.log('User list refreshed successfully');
+    } catch (error) {
+        console.error('Error refreshing user list:', error);
+    }
+});
+
+fileInput.addEventListener('change', handleFileSelection);
+let timeStamp = "examplekey123456";
+function handleFileSelection(event) {
+    const files = event.target.files;
+    if (files.length > 0) {
+        const file = files[0];
+        const formData = new FormData();
+        formData.append('pathForLoadFile', file.name);
+        formData.append('cryptoAlgorithm', selectedAlgorithm);
+        formData.append('padding', padding);
+        formData.append('encryptionMode', encryptionMode);
+        formData.append('content', "");
+        formData.append('format', file.name.split('.').pop().toLowerCase());
+        formData.append('key', sharedKeyAlice);
+
+        fetch('http://localhost:8090/upload', {
+            method: 'POST',
+            body: formData,
+            mode: 'no-cors'
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log('File uploaded successfully');
+                } else {
+                    console.error('Failed to upload file');
+                }
+            })
+            .catch(error => {
+                console.error('Error uploading file:', error);
+            });
+    }
+}
+
 function onLogout() {
-    stompClient.send("/app/user.disconnectUser",
-        {},
-        JSON.stringify({ nickName: nickname, fullName: fullname, status: 'OFFLINE' })
-    );
+    if (stompClient) {
+        stompClient.send("/app/user.disconnectUser",
+            {},
+            JSON.stringify({nickName: nickname, fullName: fullname, status: 'OFFLINE'})
+        );
+    }
     window.location.reload();
 }
+
+window.addEventListener('beforeunload', async function(event) {
+    if (stompClient) {
+        stompClient.send("/app/user.disconnectUser",
+            {},
+            JSON.stringify({nickName: nickname, fullName: fullname, status: 'OFFLINE'})
+        );
+    }
+});
+
 
 usernameForm.addEventListener('submit', connect, true);
 messageForm.addEventListener('submit', sendMessage, true);
 logout.addEventListener('click', onLogout, true);
-window.onbeforeunload = () => onLogout();
